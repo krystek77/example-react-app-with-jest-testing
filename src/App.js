9194179c64f2a12ff6218340cc2ae5a9f5abd2b7
krystek77 <krystian.wrona77@gmail.com>
2020-08-21 08:28:28 +0200

    add counter handler

--- src/App.js
+++ src/App.js
@@ -6,11 +6,47 @@ const URL_API = 'http://hn.algolia.com/api/v1/search';
 
 function App() {
   const [counter, setCounter] = React.useState(0);
+  const [errorCounter, setErrorCounter] = React.useState(null);
+
+  function handleCounter(operationType) {
+    switch (operationType) {
+      case 'inc':
+        setCounter((oldCounter) => oldCounter + 1);
+        break;
+      case 'dec':
+        setCounter((oldCounter) => oldCounter - 1);
+        break;
+      default:
+        const error = {
+          message: `Opeartion '${operationType}' not allowed`,
+        };
+        setErrorCounter(error);
+    }
+  }
   return (
     <div className='App'>
       <h1>Jest Testing in React</h1>
       <h2>Counter</h2>
       <Counter counter={counter} />
+      <div className='btn-group'>
+        <button
+          className='btn btn-increment'
+          data-testid='inc'
+          onClick={() => handleCounter('inc')}
+        >
+          Increment by 1
+        </button>
+        <button
+          className='btn btn-decrement'
+          data-testid='dec'
+          onClick={() => handleCounter('dec')}
+        >
+          Decrement by 1
+        </button>
+      </div>
+      {errorCounter && (
+        <div className='error-counter'>{errorCounter.message}</div>
+      )}
       <h2>Fetching data from Hacker News</h2>
     </div>
   );
