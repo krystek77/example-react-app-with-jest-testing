f480cfc7237780c24b76ca117d9e45d0a6e95f36
krystek77 <krystian.wrona77@gmail.com>
2020-08-21 08:07:58 +0200

    add Counter to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Counter from './Counter';
 import axios from 'axios';
 
 const URL_API = 'http://hn.algolia.com/api/v1/search';
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <h1>Jest Testing in React</h1>
       <h2>Counter</h2>
+      <Counter count={0} />
       <h2>Fetching data from Hacker News</h2>
     </div>
   );
