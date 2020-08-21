0f0ad7c3e92db0a267b7be3ebb1985234d634481
krystek77 <krystian.wrona77@gmail.com>
2020-08-21 08:10:48 +0200

    add counter state

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import axios from 'axios';
 const URL_API = 'http://hn.algolia.com/api/v1/search';
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
   return (
     <div className='App'>
       <h1>Jest Testing in React</h1>
       <h2>Counter</h2>
-      <Counter counter={0} />
+      <Counter counter={counter} />
       <h2>Fetching data from Hacker News</h2>
     </div>
   );
